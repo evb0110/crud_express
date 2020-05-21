0d8de86fbedf62b7a825a1e28e231732a30f2a70
evb0110 <7149553@gmail.com>
2020-05-21 10:18:43 +0300

    persistence

--- server.js
+++ server.js
@@ -2,38 +2,58 @@ const express = require('express')
 let app = express()
 app.use(express.json())
 
+const fs = require('fs')
+
 const port = 3000
-let objects = []
+
+const db = './db'
+
+const write = (objects) => new Promise ((res, rej) => {
+  fs.writeFile(db, JSON.stringify(objects), (err) => {
+    if (err) return console.warn(err)
+    res('ok')
+  })
+}) 
+const read = () => new Promise((res, rej) => {
+  fs.readFile(db, (err, data) => {
+    if (err) return console.warn(err)
+    res(JSON.parse(data))
+  })
+})
+
 
 app.get('/', (req, res) => {
-  res.send(objects);
+  read().then(d => res.send(d))
 })
 
 app.get('/:id', (req, res) => {
   const id = req.params.id
-  const obj = objects.find(o => o.id === id)
-  res.send(obj)
+  read().then(objects => objects.find(o => o.id === id))
+        .then(d => res.send(d))
 })
 
 app.post('/', (req, res) => {
   const obj = req.body
   const id = Math.random().toString().slice(2, 12)
   const newObj = {...obj, id}
-  objects = [...objects, newObj]
-  res.send(newObj)
+  read().then(objects => [...objects, newObj])
+        .then(write)
+        .then(() => res.send(newObj))
 })
 
 app.patch('/', (req, res) => {
   const obj = req.body
   const id = obj.id
-  objects = objects.map(o => o.id === id ? obj : o)
-  res.status(200).json({status:"ok"})
+  read().then(objects => objects.map(o => o.id === id ? obj : o))
+        .then(write)
+        .then(() => res.status(200).json({status:"ok"}))
 })
 
 app.delete('/:id', (req, res) => {
   const id = req.params.id
-  objects = objects.filter(o => o.id !== id)
-  res.status(200).json({status:"ok"})
+  read().then(objects => objects.filter(o => o.id !== id))
+        .then(write)
+        .then(() => res.status(200).json({status:"ok"}))
 })
 
 
