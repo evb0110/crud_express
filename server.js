68e4ac50322a35e721b43f26e35cd30a0200c336
evb0110 <7149553@gmail.com>
2020-05-28 23:03:25 +0300

    minor

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const cors = require('cors')
-let app = express()
+
+const app = express()
 app.use(express.json())
 app.use(cors())
 
@@ -8,19 +9,18 @@ const fs = require('fs')
 const port = 3000
 const db = './db.json'
 
-const read = () => new Promise((res, rej) => {
+const read = () => new Promise((res, rej) => (
   fs.readFile(db, (err, data) => {
     if (err) return console.warn(err)
     res(JSON.parse(data.toString()))
-  })
-})
+  })))
 
-const write = (objects) => new Promise ((res, rej) => {
+const write = (objects) => new Promise((res, rej) => (
   fs.writeFile(db, JSON.stringify(objects, null, 2), (err) => {
     if (err) return console.warn(err)
     res('ok')
   })
-})
+))
 
 app.get('/', (req, res) => {
   read().then(d => res.send(d))
@@ -29,7 +29,7 @@ app.get('/', (req, res) => {
 app.get('/:id', (req, res) => {
   const id = req.params.id
   read().then(objects => objects.find(o => o.id === id))
-        .then(d => res.send(d))
+    .then(d => res.send(d))
 })
 
 app.post('/', (req, res) => {
@@ -37,23 +37,23 @@ app.post('/', (req, res) => {
   const id = Math.random().toString().slice(2, 12)
   const newObj = {...obj, id}
   read().then(objects => [newObj, ...objects])
-        .then(write)
-        .then(() => res.send(newObj))
+    .then(write)
+    .then(() => res.send(newObj))
 })
 
 app.patch('/', (req, res) => {
   const obj = req.body
   const id = obj.id
   read().then(objects => objects.map(o => o.id === id ? obj : o))
-        .then(write)
-        .then(() => res.status(200).json({status:"ok"}))
+    .then(write)
+    .then(() => res.status(200).json({status: "ok"}))
 })
 
 app.delete('/:id', (req, res) => {
   const id = req.params.id
   read().then(objects => objects.filter(o => o.id !== id))
-        .then(write)
-        .then(() => res.status(200).json({status:"ok"}))
+    .then(write)
+    .then(() => res.status(200).json({status: "ok"}))
 })
 
 
